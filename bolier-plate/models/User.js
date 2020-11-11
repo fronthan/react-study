f3db10aa0e3ebdaa02c871599dafe9ff9219ffcd
fronthan <cynthia0330@kakao.com>
2020-11-11 17:56:58 +0900

    11.11 node 공부 마무리

--- bolier-plate/models/User.js
+++ bolier-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //salt 가 몇 글자인지?
+
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,6 +33,24 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function( next ){//next 하면 index.js의 user.save 부분으로 넘어간다
+    var user = this; //userSchema를 가리킨다.
+
+    if (user.isModified('password')) {
+        //비밀번호를 바꿀 때만 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){//hash가 암호화된 비번
+                if(err) return next(err);
+                user.password = hash;
+                next()
+            })
+        })
+    }
+    
+});//mongoose 메서드
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User};
