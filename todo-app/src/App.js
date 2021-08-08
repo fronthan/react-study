f0af0fb33a7ad7b2241e2aa4c75e9b89e2901608
fronthan <cynthia0330@kakao.com>
2021-08-08 23:33:45 +0900

    11.5 todoReducer 공부

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,38 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i<=2500; i++) {
+    array.push({
+      id:i,
+      text:`할 일 ${i}`,
+      checked:false,
+    });
+  }
+  return array;
+}
 
+function todoReducer(todos, action) {
+  switch(action.type) {
+    case 'INSERT' :
+      return todos.concat(action.todo);
+    case 'REMOVE' :
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE' :
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked} : todo,
+      );
+      default: return todos;
+  }
+}
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1, text:'리액트의 기초', checked:true
-    },
-    {
-      id:2, text:'컴포넌트 스타일링', checked:true
-    },
-    {
-      id:3, text:'일정 관리 앱 만들어보기', checked:false
-    }
-  ]);
+  // const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text=> {
@@ -26,26 +41,25 @@ function App() {
         text,
         checked:false
       }
-      setTodos(todos.concat(todo));
+      // setTodos(todos => todos.concat(todo));
+      dispatch({type: 'INSERT', todo});
       nextId.current +=1;
     },
-    [todos]
-  );  
+    []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    }, [todos]
-  );
+  const onRemove = useCallback(id => {
+      // setTodos(todos => todos.filter(todo => todo.id !== id));
+      dispatch({type: 'REMOVE', id});
+    }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked} :todo,),
-      );
-    }, [todos]
-  );
+  const onToggle = useCallback(id => {
+      // setTodos(todos =>
+      //   todos.map(todo =>
+      //     todo.id === id ? { ...todo, checked: !todo.checked} :todo,
+      //   ),
+      // );
+      dispatch({type: 'TOGGLE', id});
+    }, []);
 
   return (
     <TodoTemplate>
@@ -56,3 +70,6 @@ function App() {
 }
 
 export default App;
+
+// 프로젝트를 프로덕션 모드로 구동하기
+// yarn build > yarn global add serve > serve -s build
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -23,4 +23,12 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
 
 TodoListItem.propTypes = {};
 
-export default TodoListItem;
+export default React.memo(TodoListItem); //props 가 바뀌지 않으면 리렌더링되지 않도록 하는 것. React.memo
+
+/**
+ * 컴포넌트가 리렌더링이 될 때
+ * 1 자신이 전달받은 props가 변경될 때
+ * 2 자신의 state가 바뀔 때
+ * 3 부모 컴포넌트가 리렌더링 될 때
+ * 4 forceUpdate 함수가 실행될 때
+ */
