8dd4f7862fd586768239bac92345c534e9c3acf9
fronthan <cynthia0330@kakao.com>
2021-08-18 22:07:48 +0900

    13.6.4 NavLink 공부

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
@@ -9,19 +9,30 @@ const App = () => {
   return (
     <div>
       <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
+        <li> <Link to="/">홈</Link> </li>
         <li><Link to="/about">소개</Link></li>
         <li><Link to="/profiles">프로필</Link></li>
         <li><Link to="/history">history 예제</Link></li>
       </ul>
-<hr />
-      <Route path="/" exact={true} component={Home} />
-      <Route path={['/about', '/info']} component={About}/>
-      {/* <Route path="/profile/:username" component={Profile} /> */}
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <hr />
+
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path={['/about', '/info']} component={About}/>
+        {/* <Route path="/profile/:username" component={Profile} /> */}
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route render={({ location })=> (
+          <div>
+            <h2>이 페이지는 존재하지 않습니다.:</h2>
+            <p>{location.pathname}</p>
+          </div>
+        )}
+        />
+      </Switch>
+      {/* Switch 컴포넌트는 여러 Route를 감싸서 그 중 일치하는 단 하나의 라우트만을 렌더링시켜준다.
+        모든 규칙과 일치하지 않을 때 보여줄 Not Found 페이지도 위와같이 구현한다
+      */}
     </div>
   );
 };
--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 // import WithRouterSample from './WithRouterSample'; 
 
 //서브라우트 만들기 : 라우트로 사용되고 있는 컴포넌트 내부에 route 컴포넌트를 또 사용한다
 const Profiles = () => {
+  const activeStyle = {
+    background: 'black',
+    color: 'white'
+  }
+
   return (
     <div>
       <h3>사용자 목록 : </h3>
       <ul>
         <li>
-          <Link to="/profiles/velopert">민정</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/velopert" active>민정</NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">까비</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">까비</NavLink>
         </li>
       </ul>
+      {/* NavLink 컴포넌트에서 activeStyle={activeStyle} 를 props에 넣으면 활성화 시 active를 자동으로 적용해준다 */}
 
       <Route path="/profiles" exact
        render={()=> <div>사용자를 선택해주세요.</div>} />
