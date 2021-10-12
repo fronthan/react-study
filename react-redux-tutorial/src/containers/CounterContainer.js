4464a9917ac1a71eba3d27c739c381bf9f51042a
fronthan <cynthia0330@kakao.com>
2021-10-12 21:41:32 +0900

    17.7 useSelector

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter number={number}/>
     );
 };
 
-/*const mapStateToProps = state => ({
-    number: state.counter.number,
-}); 
-
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-    //    console.log('increase');
-        dispatch(increase());
-    },
-    decrease: () => {
-    //    console.log('decrease');
-        dispatch(decrease());
-    }
-}); 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(CounterContainer);
-//아래처럼 connect 함수 안에 익명함수 형태로 선언 하는 수도 있다. 
-*/
-
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    dispatch => 
-    bindActionCreators(
-        {
-            increase, decrease,
-        },
-        dispatch
-    )
-)(CounterContainer);
+export default CounterContainer;
