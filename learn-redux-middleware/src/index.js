ae2b349ac93e0ab89deca86e1b533848fc9b5ea5
fronthan <cynthia0330@kakao.com>
2021-10-13 22:37:31 +0900

    18장 3.1.4 리덕스 미들웨어, 웹요청 비동기작업처리

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -7,9 +7,11 @@ import './index.css';
 import App from './App';
 
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -6,11 +6,24 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+//1초 뒤에 increase 혹은 decrease 함수를 디스패치한다
+export const increaseAsync = () => dispatch => {
+  setTimeout(()=> {
+    dispatch(increase());
+  }, 1000);
+}
+
+export const decreaseAsync = () => dispatch => {
+  setTimeout(()=> {
+    dispatch(decrease());
+  }, 1000);
+}
+
 const initialState = 0; //상태는 객체가 아니어도 된다
 
 const counter = handleActions(
   {
-    [INCREASE]: state=> state+1,
+    [INCREASE]: state => state+1,
     [DECREASE]: state => state-1
   },
   initialState
