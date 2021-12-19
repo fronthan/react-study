a883edd95a1a893b8cb10a046f29482cd692f4af
fronthan <cynthia0330@kakao.com>
2021-12-19 22:18:48 +0900

    23.2.3 redux-saga 로 회원가입 구현

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -53,17 +53,37 @@ const ButtonWithMarginTop = styled(Button)`
   margin-top:1rem;
 `;
 
-const AuthForm = () => {
+const textMap = {
+  login: '로그인',
+  register: '회원가입'
+};
+
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
+  const text = textMap[type];
+
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
-      <form>
-        <StyledInput autoComplete="username" name="username" placeholder="아이디" />
-        <StyledInput autoComplete="new-password" name="password" placeholder="비밀번호" type="password" />
-        <ButtonWithMarginTop lime fullWidth>로그인</ButtonWithMarginTop>
+      <h3>{text}</h3>
+      <form onSubmit={onSubmit}>
+        <StyledInput autoComplete="username" name="username" placeholder="아이디"
+          onChange={onChange} value={form.username}
+         />
+        <StyledInput autoComplete="new-password" name="password" placeholder="비밀번호" type="password" 
+          onChange={onChange} value={form.password}
+        />
+        { type === 'register' && (
+          <StyledInput autoComplete="new-password" name="passwordConfirm" placeholder="비밀번호 확인" type="password"
+            onChange={onChange} value={form.passwordConfirm}
+           />
+        )}
+        <ButtonWithMarginTop lime fullWidth>{text}</ButtonWithMarginTop>
       </form>
       <Footer>
+      {type === 'login'? (
         <Link to="/register">회원가입</Link>
+      ) : (
+        <Link to="/login">로그인</Link>
+      )}
       </Footer>
     </AuthFormBlock>
   );
